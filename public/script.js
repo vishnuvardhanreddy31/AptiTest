e93483a2e8c3988f24a9031c63c4f34abce4377e
pranay <pranaydavath1416@gmail.com>
2023-05-15 22:48:27 +0530

    test

--- public/script.js
+++ public/script.js
@@ -1,4 +1,5 @@
 // Import the functions you need from the SDKs you need
+// version 0.0.5
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-analytics.js";
 import {
